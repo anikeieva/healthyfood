92844cbd88dfb17b0f0c35514ded24c9fd44e00c
Alyona Anikeieva <alyonaoooooooo@gmail.com>
2018-01-22 22:25:31 +0200

    was added new changed and script

---
+++ app/js/from_images_to_images.js
@@ -0,0 +1,30 @@
+function ready() {
+  var welcomeRight = document.getElementsByClassName('welcome-right')[0];
+  var biggerWelcomeTasty = document.getElementsByClassName('bigger_welcome_tasty')[0];
+  var smallerWelcomeTasty = document.getElementsByClassName('smaller_welcome_tasty')[0];
+
+  function newImages() {
+    biggerWelcomeTasty.style.backgroundImage = 'url(../img/main3.jpg)';
+    smallerWelcomeTasty.style.backgroundImage = 'url(../img/4.jpg)';
+  }
+
+  function oldImages() {
+    biggerWelcomeTasty.style.backgroundImage = 'url(../img/hf.jpg)';
+    smallerWelcomeTasty.style.backgroundImage = 'url(../img/drink.jpg)';
+  }
+
+  var counter = 1;
+
+  var timerId = setInterval(function() {
+    counter++;
+    if(counter % 2 === 0) {
+      newImages();
+    }
+    else {
+      oldImages();
+    }
+  }, 3000);
+
+}
+
+document.addEventListener("DOMContentLoaded", ready);
