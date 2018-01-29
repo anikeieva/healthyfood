52f98dddf3975a68e438153894a221af0d350b35
anikeieva <alyonaoooooooo@gmail.com>
2018-01-29 23:00:37 +0200

    was added closing icon and animation to menu's button

--- app/js/sideBarMenu.js
+++ app/js/sideBarMenu.js
@@ -1,10 +1,14 @@
 function ready() {
   var toggleBtn = document.getElementsByClassName('toggle-btn')[0];
+  var menu_icon = document.getElementById('menu-icon');
+  var sideBar = document.getElementById('sideBar');
 
   function activeMenu() {
-    var sideBar = document.getElementById('sideBar');
 
     sideBar.classList.toggle('active');
+    menu_icon.classList.toggle('fa-bars');
+    menu_icon.classList.toggle('fa-times');
+
   }
 
   toggleBtn.addEventListener('click', activeMenu);
